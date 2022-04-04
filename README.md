1ba595363b894bb6b1e0a81b5112f6a75092b95c
Canário (or Canary) <81337238+Canario0001@users.noreply.github.com>
2022-04-03 22:23:54 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Coin Clicker
+#### yes, very creative.
+
+## Português
+
+Este é o meu primeiro projeto de jogo com HTML e JS. Fiz em menos de 2 dias, e ainda tenho que aprender mais sobre CSS. O jogo é um jogo incremental onde você é um empreendedor e seu objetivo é comprar uma BIG Tech. Se divirta!
+
+## English
+
+This is my first game project with HTML and JS. I did in less than 2 days, and I have to learn more about CSS. The game is a clicker game which you are a businessman and buying one BIG Tech is your goal. Enjoy!
---
+++ src/main.js
@@ -0,0 +1,127 @@
+// Código criado por Canario0001.
+// A distribuição e modificação é permitida contanto que não venda ou passe como sua.
+console.log('%c < -- Hacker safado! Não mexa nas moedas. -- >', 'color: green')
+
+var moedas = 0
+var proleCusto = 950
+var prole = 0
+var minaOuro = 0
+var minaOuroCusto = 3000
+var banco = 0
+var bancoCusto = 320000
+var bigTech = 0
+var bigTechCusto = 80000000000000
+
+window.onload = function() {
+    load()
+}
+window.onbeforeunload = function() {
+    save()
+}
+
+function addMoeda(quantia) {
+    moedas += quantia
+    document.getElementById('moeda').innerHTML = moedas
+}
+
+function buyProle() {
+    if (moedas >= proleCusto) {
+        moedas -= proleCusto
+        prole += 1
+        proleCusto = Math.round(proleCusto * 1.10)
+
+        document.getElementById('moeda').innerHTML = moedas
+        document.getElementById('prole').innerHTML = prole
+        document.getElementById('sal').innerHTML = proleCusto
+    }
+    else {
+        alert('Você não tem moedas para comprar.')
+    }
+}
+
+function buyMina() {
+    if (moedas >= minaOuroCusto) {
+        moedas -= minaOuroCusto
+        minaOuro += 1
+        minaOuroCusto = Math.round(minaOuroCusto * 1.10)
+
+        document.getElementById('moeda').innerHTML = moedas
+        document.getElementById('mina-qua').innerHTML = minaOuro
+        document.getElementById('preco-mina').innerHTML = minaOuroCusto
+    }
+    else {
+        alert('Você não tem moedas para comprar.')
+    }
+}
+
+function buyBanco() {
+    if (moedas >= bancoCusto) {
+        moedas -= bancoCusto
+        banco += 1
+        bancoCusto = Math.round(bancoCusto * 1.20)
+
+        document.getElementById('moeda').innerHTML = moedas
+        document.getElementById('qbank').innerHTML = banco
+        document.getElementById('cbank').innerHTML = bancoCusto
+    }
+    else {
+        alert('Você não tem moedas para comprar.')
+    }
+}
+
+function buyEmpresa() {
+    if (moedas >= 80000000000000) {
+        moedas -= bigTechCusto
+        bigTech += 1
+
+        document.getElementById('moeda').innerHTML = moedas
+        document.getElementById('empres').innerHTML = bigTech
+        document.getElementById('pc-gigante').innerHTML = bigTechCusto
+
+        alert('Parabéns! Você ganhou. Pode continuar jogando, se quiser.')
+    }
+    else {
+        alert('Você não tem moedas para comprar.')
+    }
+}
+
+setInterval(function() {
+    addMoeda(prole)
+    addMoeda(minaOuro*50)
+    addMoeda(banco*2000)
+    document.getElementById('moeda').innerHTML = moedas
+    document.title = moedas + " moedas - Empreendedorismo Ultimate Pro Max"
+    MoedasPS()
+}, 1000)
+
+function MoedasPS() {
+    document.getElementById('mps').innerHTML = prole + (minaOuro * 50) + (banco * 2000)
+}
+
+function save() {
+    // q -> quantidade
+    // c -> custo
+    var config = {
+        qmoeda: moedas,
+        qworker: prole,
+        cworker: proleCusto,
+        qmina: minaOuro,
+        cmina: minaOuroCusto,
+        qbanco: banco,
+        cbanco: bancoCusto
+    }
+    localStorage.setItem("settings", JSON.stringify(config))
+}
+
+function load() {
+    // q -> quantidade
+    // c -> custo
+    var baz = JSON.parse(localStorage.getItem("settings"))
+    if (typeof baz.qmoeda !== undefined) moedas = baz.qmoeda
+    if (typeof baz.qworker !== undefined) prole = baz.qworker
+    if (typeof baz.cworker !== undefined) proleCusto = baz.cworker
+    if (typeof baz.qmina !== undefined) minaOuro = baz.qmina
+    if (typeof baz.cmina !== undefined) minaOuroCusto = baz.cmina
+    if (typeof baz.qbanco !== undefined) banco = baz.qbanco
+    if (typeof baz.cbanco !== undefined) bancoCusto = baz.cbanco
+}
